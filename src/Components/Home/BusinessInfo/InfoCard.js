e414a44bc206c1c83a8725f71eff91c4fe4fd432
Siful <siful@Sifuls-MacBook-Pro.local>
2020-10-24 11:19:40 +0600

    files added

--- src/Components/Home/BusinessInfo/InfoCard.js
+++ src/Components/Home/BusinessInfo/InfoCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 
 const infoCard = (props) => {
-    const{title, des, icon, background} = props.info;
+    const{title, des, background} = props.info;
     return (
         <Col lg="4" style={{backgroundColor: background}}>
               <h4>{title}</h4>  
