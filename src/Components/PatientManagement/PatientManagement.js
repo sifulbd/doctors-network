cd5b865cbd0317b6bf6f0ca55a3f47c4fb2e3943
Siful <sifulsmm@gmail.com>
2020-10-29 19:12:55 +0600

    no adding

--- src/Components/PatientManagement/PatientManagement.js
+++ src/Components/PatientManagement/PatientManagement.js
@@ -3,19 +3,19 @@ import { Form } from "react-bootstrap";
 import { initialPatient, patientReducer } from "./PatientReducer";
 
 export default function PatientManagement() {
-    const [state, dispatch] = useReducer(patientReducer, initialPatient);
-    const nameRef = useRef();
+  const [state, dispatch] = useReducer(patientReducer, initialPatient);
+  const nameRef = useRef();
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        dispatch({
-            type: 'ADD_PATIENT',
-            name: nameRef.current.value,
-            id: state.patients.length + 1
-        })
-        nameRef.current.value = ''
+  const handleSubmit = event => {
+      event.preventDefault();
+      dispatch({
+          type: 'ADD_PATIENT',
+          name: nameRef.current.value,
+          id: state.patients.length + 1
+      })
+      nameRef.current.value = ''
 
-    }
+  }
   return (
     <div className="">
       <h1>Patient Management {state.patients.length ? state.patients.length : 0}</h1>
